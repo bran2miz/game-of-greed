6f72dd3db3aaf46bed959fffd58c4e6951056e29
brandonmizutani <bran2miz@gmail.com>
2021-12-22 21:39:17 -0800

    11 tests passed

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 https://stackoverflow.com/questions/12229064/mapping-over-values-in-a-python-dictionary
 
 https://docs.python.org/3/library/collections.html
+
+https://en.wikipedia.org/wiki/Dice_10000
--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -1,25 +1,6 @@
 import random
 from collections import Counter
 
-# 5’s = 50 points
-# 1’s= 100 points
-
-# 1,1,1 = 300 points
-# 2,2,2 = 200 points
-# 3,3,3 = 300 points
-# 4,4,4 = 400 points
-# 5,5,5 = 500 points
-# 6,6,6 = 600 points
-
-# Four of a Kind = 1,000 points
-# Five of a Kind = 2,000 points
-# Six of a Kind = 3,000 points
-# A Straight of 1-6 = 1,500 points
-
-# Three Pairs = 1,500 points
-# Four of a Kind + a Pair = 1,500
-
-# Two sets of Three of a Kind = 2,500
 
 
 # Calculate Score -
@@ -27,47 +8,41 @@ from collections import Counter
 #   output: score from the roll
 # what values could we tuple to find dice roll
 
-# farkle = {(1,): 100),
-#         ((1, 1): 200),
-#         ((1, 1, 1): 1000),
-#         ((1, 1, 1, 1): 2000),
-#         ((1, 1, 1, 1, 1): 3000),
-#         ((1, 1, 1, 1, 1, 1): 4000),
-#         ((2,): 0),
-#         ((2, 2): 0),
-#         ((2, 2, 2): 200),
-#         ((2, 2, 2, 2): 400),
-#         ((2, 2, 2, 2, 2): 600),
-#         ((2, 2, 2, 2, 2, 2): 800),
-#         ((3,): 0),
-#         ((3, 3): 0),
-#         ((3, 3, 3): 300),
-#         ((3, 3, 3, 3): 600),
-#         ((3, 3, 3, 3, 3): 900),
-#         ((3, 3, 3, 3, 3, 3): 1200),
-#         ((4,): 0),
-#         ((4, 4): 0),
-#         ((4, 4, 4): 400),
-#         ((4, 4, 4, 4): 800),
-#         ((4, 4, 4, 4, 4): 1200),
-#         ((4, 4, 4, 4, 4, 4): 1600),
-#         ((5,): 50),
-#         ((5, 5): 100),
-#         ((5, 5, 5): 500),
-#         ((5, 5, 5, 5): 1000),
-#         ((5, 5, 5, 5, 5): 1500),
-#         ((5, 5, 5, 5, 5, 5): 2000),
-#         ((6,): 0),
-#         ((6, 6): 0),
-#         ((6, 6, 6): 600),
-#         ((6, 6, 6, 6): 1200),
-#         ((6, 6, 6, 6, 6): 1800),
-#         ((6, 6, 6, 6, 6, 6): 2400),
-#         ((1, 2, 3, 4, 5, 6): 1500),
-#         ((2, 2, 3, 3, 4, 6): 0),
-#         ((2, 2, 3, 3, 6, 6): 1500),
-#         ((1, 1, 1, 2, 2, 2): 1200)
-# }
+# Straight 1-6	1,500
+# Three Pairs	1,000 (2x2's, 2x3's, 2x4's, etc)
+# 3x6's	600
+# 4x6's	1,200
+# 5x6's	2,400
+# 6x6's	4,800
+# unless rolled at one time then its automatic 10,000
+# 3x5's	500
+# 4x5's	1,000
+# 5x5's	2,000
+# 6x5's	4,000
+# unless rolled at one time then its automatic 10,000
+# 3x4's	400
+# 4x4's	800
+# 5x4's	1,600
+# 6x4's	3,200
+# unless rolled at one time then its automatic 10,000
+# 3x3's	300
+# 4x3's	600
+# 5x3's	1,200
+# 6x3's	2,400
+# unless rolled at one time then its automatic 10,000
+# 3x2's	200
+# 4x2's	400
+# 5x2's	800
+# 6x2's	1,600
+# unless rolled at one time then its automatic 10,000
+# 3x1's	1,000
+# 4x1's	2,000
+# 5x1's	4,000
+# 6x1's	8,000
+# unless rolled at one time then its automatic 10,000
+
+# If a player rolls all 6 of the same dice in 1 roll the game is automatically over no player gets a last roll.
+# Double Trips when 2 sets of 3 of a kind are hit. Scores are added together and doubled.
 
 
 
@@ -83,10 +58,15 @@ class GameLogic:
       # for i in game_start(1,7):
       #   if i == 1:
       #     tally_score += 50
-      if dice_number[5] == 1:
+      if dice_number[5] == 1 or dice_number[5]== 2:
         tally_score += 50 * dice_number[5]
       
+      if dice_number[1] == 1 or dice_number[1] == 2:
+        tally_score += 100 * dice_number[1]
+
       return tally_score
+
+      
 
     @staticmethod
     def roll_dice(rolled_dice):
