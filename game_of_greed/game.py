6ac497c06a7ad501a7b1c166ee51ff0054ad7746
brandonmizutani <bran2miz@gmail.com>
2022-01-02 17:45:51 -0800

    finished test 1 & 2 and starting test 3

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -4,14 +4,14 @@ from game_of_greed.game_logic import GameLogic
 
 class Game:
 
-  def __init__(self, rounds = 1, dice_quantity = 6):
+  def __init__(self, rounds =1, dice_quantity = 6):
     self.banker = Banker()
     self.status = True
     self.number_of_rounds = rounds
-    self.roll_dice = GameLogic.roll_dice
     self.dice_quantity = dice_quantity
 
-  def play(self, roller = None):
+  def play(self, roller = GameLogic.roll_dice):
+    self.roll_dice = roller
     print("Welcome to Game of Greed")
     print("(y)es to play or (n)o to decline")
     player_input = input("> ")
@@ -23,18 +23,52 @@ class Game:
 
   def round_start(self):
     while self.status and self.banker.balance <= 10000:
-      dice_quantity = 6
+      dice_quantity = 6    
       while self.status:
-        print("Starting round {self.number_of_rounds}")
-        print("Rolling {self.dice_quantity} dice...")
-        dice_string = ''.join(str(num) for num in (self.roll_dice(self.dice_quantity)))
+        print(f"Starting round {self.number_of_rounds}")
+        print(f"Rolling {self.dice_quantity} dice...")
+        dice_string = ' '.join(str(num) for num in (self.roll_dice(dice_quantity)))
         print(f"*** {dice_string} ***")
         print(f"Enter dice to keep, or (q)uit:")
-        quit_game = input("> ")
+        game_input = input("> ")
+# ** 1 1 4 1 3 2 **
+# 1 1 1 2
+        if game_input == "q":
+          print(f"Thanks for playing. You earned {self.banker.balance} points")
+          self.status = False
+        else:
+          get_score = GameLogic.calculate_score(game_input)
+          self.banker.shelf(get_score)
+          print(f"You have {self.banker.shelf} unbanked points and {dice_quantity} dice remaining")
+          print("(r)oll again, (b)ank your points or (q)uit:")
+          roll_again = input("> ")
+          if roll_again == "r":
+            print(f"Rolling {self.dice_quantity} dice...")
+            continue
+          if roll_again == "b":
+            self.banker.bank()
+            print(f"You banked {get_score} points in round {self.number_of_rounds}")
+            self.number_of_rounds += 1
+            self.banker.clear_shelf()
+            break
+          if roll_again == "q":
+            pass
 
-        if quit_game == 'q':
-          print("Thanks for playing. You earned {self.banker.balance} points")
      
+# You banked 50 points in round 1
+# Total score is 50 points
+     
+# Banked points
+# (r)oll again, (b)ank your points or (q)uit:
+# user input
+# total score
+# start round 2
+# Rolling {self.dice_quantity} dice...
+# shows dice 
+# Enter dice to keep, or (q)uit:
+# user input
+# Thanks for playing. You earned {}} points
+
 
 
 
@@ -44,27 +78,3 @@ class Game:
 if __name__ == "__main__":
   play_game = Game()
   play_game.play()
-
-
-
-
-
-
-
-
-# Application should simulate rolling between 1 and 6 dice
-    # rolling function - everything needs access to this function
-    # for print: concatenate dice #s with the ***
-
-
-# Application should allow user to set aside dice each roll
-    # shelving dice 
-  
-# Application should keep track of current round
-    # rounds need a start point then increase after each roll
-
-# Application should allow 'banking' current score or rolling again.
-# Application should keep track of total score
-
-# Application should implement all features from previous version
-# Application should have automated tests to ensure proper operation
