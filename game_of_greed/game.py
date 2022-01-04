93b4d7f026335e02e5bfe48df5edc346a13f5288
Clarissa P <clarissapamonicutt@gmail.com>
2022-01-03 16:34:36 -0800

    worked on code

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -1,75 +1,97 @@
 # test merge
-
-
+# from random import choice, randit
+import sys
 from game_of_greed.banker import Banker
 from game_of_greed.game_logic import GameLogic
 # from game_of_greed.game_logic import GameLogic
 
 class Game:
 
-  def __init__(self, rounds =1, dice_quantity = 6):
+  def __init__(self):
     self.banker = Banker()
     self.status = True
-    self.number_of_rounds = rounds
-    self.dice_quantity = dice_quantity
+    self.number_of_rounds = 0
+    self.dice_quantity = 6
 
-  def play(self, roller = GameLogic.roll_dice):
+  def default_roller(self):
+    GameLogic.roll_dice(self.dice_quantity)
+
+  def play(self, roller= GameLogic.roll_dice):
     self.roll_dice = roller
     print("Welcome to Game of Greed")
     print("(y)es to play or (n)o to decline")
-    player_input = input("> ")
-    if player_input == 'n':
-      print('OK. Maybe another time')
-    elif player_input == 'y':
-        self.dice_quantity = 6
-        self.round_start()
+    while True:
+      player_input = input("> ")
+      if player_input == 'n':
+        print('OK. Maybe another time')
+      elif player_input == 'y':
+        # self.dice_quantity = 6
+        self.round_start(roller)
 
-  def round_start(self):
+  def round_start(self, roller):
     while self.status and self.banker.balance <= 10000:
-      while self.status:
-        print(f"Starting round {self.number_of_rounds}")
-        print(f"Rolling {self.dice_quantity} dice...")
+      self.number_of_rounds += 1
+      # while self.status:
+      print(f"Starting round {self.number_of_rounds}")
+      print(f"Rolling {self.dice_quantity} dice...")
 # need to save the result from self.roll_dice to use in gamelogic.calulate_score
 # dicerolled is the result of rolling the dice, used in dice_string and get_score
-        dicerolled = self.roll_dice(self.dice_quantity)
-        dice_string = ' '.join(str(num) for num in dicerolled)
-        print(f"*** {dice_string} ***")
-        print(f"Enter dice to keep, or (q)uit:")
-        
-        game_input = input("> ")
+      dicerolled = roller(self.dice_quantity)
+      dice_string = ''
+      for num in dicerolled:
+        dice_string += str(num) + " "
+      dice_string = ' '.join(str(num) for num in dicerolled)
+      print(f"*** {dice_string}***")
+
+
+      # if GameLogic.calculate_score(dicerolled) == 0:
+      #   print("Farkled")
+      #   continue
+
+      print(f"Enter dice to keep, or (q)uit:")  
+      game_input = input("> ")
       # quitting
-        if game_input == "q":
-          print(f"Thanks for playing. You earned {self.banker.balance} points")
-          self.status = False
+      if game_input == "q":
+        print(f"Thanks for playing. You earned {self.banker.balance} points")
+        # self.status = False
+        sys.exit()
         
       # continuing the game
-        else:
+      else:
+        user_entry = tuple(map(int, list(game_input)))
           # print('test line 1')
-          get_score = GameLogic.calculate_score(dicerolled)
+        self.dice_quantity -= len(game_input)
+        get_score = GameLogic.calculate_score(user_entry)
           # print('test line 2')
-          self.banker.shelf(get_score)
+        self.banker.shelf(get_score)
 # calculating dice quantity after user selects their dice
-          self.dice_quantity -= len(game_input)
-          print(f"You have {get_score} unbanked points and {self.dice_quantity} dice remaining")
-          print("(r)oll again, (b)ank your points or (q)uit:")
+        
+        print(f"You have {self.banker.shelved} unbanked points and {self.dice_quantity} dice remaining")
+        print("(r)oll again, (b)ank your points or (q)uit:")
           
       # rolling again
-          roll_again = input("> ")
-          if roll_again == "r":
-            self.number_of_rounds += 1
+        roll_again = input("> ")
+
+        if roll_again == "r":
+          # self.number_of_rounds += 1
+          # if self.dice_quantity == 0:
+          #   self.dice_quantity = 6
             # print(f"Rolling {self.dice_quantity} dice...")
-            continue
+          continue
 
       # banking the points
-          if roll_again == "b":
-            self.banker.bank()
-            print(f"You banked {get_score} points in round {self.number_of_rounds}")
-            self.banker.clear_shelf()
-            self.number_of_rounds += 1
-            break
+        elif roll_again == "b":
+          self.banker.bank()
+          self.dice_quantity = 6
+          print(f"You banked {get_score} points in round {self.number_of_rounds}")
+          print(f"Total score is {self.banker.balance} points")
+          # self.banker.clear_shelf()
+          # self.number_of_rounds += 1
+          # break
 
-          if roll_again == "q":
-            break
+        elif roll_again == "q":
+          print(f"Thanks for playing. You earned {self.banker.balance} points")
+          sys.exit()
 
 #   Fixed - 
 # number of dice to keep is entered and points are not saved or displayed
